28ec7f0d51759190a52bc67022bd92c7c0c237ba
danhuang <danhuang@192.168.1.87>
2016-10-01 19:37:14 +0200

    create C32, C32Test

---
+++ src/test/java/Dan/Huang/SVC/miw/upm/es/C32Test.java
@@ -0,0 +1,18 @@
+package Dan.Huang.SVC.miw.upm.es;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class C32Test {
+	private C32 c32;
+	@Before
+	public void before() {
+		c32 = new C32();
+	}
+	@Test
+	public void testMA() {
+		assertEquals("mA", c32.mA());
+	}
+}
